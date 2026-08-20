4f03d44c634649e5024daddff208fa34c37b7f73
Lochana39 <sulochanalochana6@gmail.com>
2026-08-20 23:18:55 +0530

    lint-error-fix

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -3,12 +3,12 @@ export default function decorate(block) {
   cards.forEach((card) => {
     card.classList.add('customcard');
     const number = card.querySelector('h2');
-    number?.classList.add('customcard__number');
+    number?.classList.add('customcard-number');
     const image = card.querySelector('p:has(img)');
-    image?.classList.add('customcard__image');
+    image?.classList.add('customcard-image');
     const description = card.querySelector('p:not(:has(img))');
-    description?.classList.add('customcard__description');
+    description?.classList.add('customcard-description');
     const title = card.querySelector('h3');
-    title?.classList.add('customcard__title');
+    title?.classList.add('customcard-title');
   });
 }
